990cf22a23092c09bcb2ca469989899f63c30d49
Jacopo De maio <jajodemaio@gmail.com>
2024-06-25 12:20:12 +0200

    Initial commit

---
+++ Spring-Web-2/src/test/java/JacopoDeMaio/Spring_Web_2/SpringWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package JacopoDeMaio.Spring_Web_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
